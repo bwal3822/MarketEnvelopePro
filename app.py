4912fb5d3f358236c2ab0b9af7f2c116f598d3b2
bwal3822 <bwaldo1@gmail.com>
2023-08-28 17:01:27 -0500

    modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # This is the start of MarketEnvelopePro 
 import numpy as np
-
+import pandas as pd
 # Load the "Data" sheet into a DataFrame
-data_df = pd.read_excel('/mnt/data/MES2022Algo.xlsx', sheet_name='Data').iloc[2:].reset_index(drop=True)
+data_df = pd.read_excel('/mnt/data/MES2022Algo.xlsx', sheet_name='Data').iloc[2:] \
+    .reset_index(drop=True)
 
 # Drop NaN columns and rows
 data_df = data_df.dropna(how='all', axis=1)
@@ -10,16 +11,19 @@ data_df = data_df.dropna(how='all', axis=0).reset_index(drop=True)
 
 # Convert relevant columns to appropriate data types
 data_df['Date'] = pd.to_datetime(data_df['Date'])
-data_df[['Open', 'High', 'Low', 'Close']] = data_df[['Open', 'High', 'Low', 'Close']].astype(float)
+data_df[['Open', 'High', 'Low', 'Close']] = data_df[['Open', 'High', 'Low', 'Close']] \
+    .astype(float)
 
 # Initialize new columns
 data_df['Direction'] = None
 data_df['Decline'] = np.nan
 
 # Calculate "Direction" based on Close prices
-data_df['Direction'] = data_df['Close'].diff().apply(lambda x: 'Up' if x > 0 else ('Down' if x < 0 else 'Sideways'))
+data_df['Direction'] = data_df['Close'].diff().apply(lambda x: 'Up' if x > 0 else \
+                                                     ('Down' if x < 0 else 'Sideways'))
 
-# Identify Buy, Sell, and Short Days (assuming the cycle starts at the first row, i.e., index 0)
+# Identify Buy, Sell, and Short Days (assuming the cycle starts at the first row, \
+# i.e., index 0)
 cycle_mapping = {0: 'Buy Day', 1: 'Sell Day', 2: 'Short Day'}
 data_df['Cycle Day'] = [cycle_mapping[i % 3] for i in range(len(data_df))]
 
